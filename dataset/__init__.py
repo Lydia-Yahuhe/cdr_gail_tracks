fc61c0a9a8c4d0a143b478d8cc89f6b668f2e9b1
Lydia-Yahuhe <zhangkainone@hotmail.com>
2021-11-25 20:28:44 +0800

    No.0: Real data will be used in this version or branch.

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,7 +1,12 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
 import seaborn as sns
+
+from fltenv.cmd import CmdCount
+from fltsim.utils import convert_with_align
 
 
 def kl_divergence(p, q):
@@ -24,61 +29,18 @@ def get_kde(x, data_array, bandwidth=0.1):
 
 
 def get_pdf_or_kde(input_array, bins):
-    print(input_array)
     bandwidth = 1.05 * np.std(input_array) * (len(input_array) ** (-1 / 5))
-    x_array = np.linspace(0, bins-1, num=100)
+    x_array = np.linspace(0, bins - 1, num=100)
     y_array = [get_kde(x_array[i], input_array, bandwidth) for i in range(x_array.shape[0])]
     return y_array
 
 
-def read_policy_from_npz(path, num_action=2):
-    policy = np.load(path)
-    acs = policy['acs']
-
-    print(path, acs.shape, np.mean(policy['rews']))
-
-    acs_step = [[] for _ in range(num_action+1)]
-    pointer = 0
-    for idx in policy['indexes']:
-        for i, ac in enumerate(acs[pointer:pointer+idx]):
-            acs_step[i].append(int(ac))
-        pointer += idx
-
-    acs_step[-1] = acs
-    return acs_step
-
-
-def visual_action_distribution():
-    bins = 108
-    num_action = 2
-
-    dqn_acs_step = read_policy_from_npz('dqn_policy.npz', num_action=num_action)
-    print(dqn_acs_step)
-    gail_acs_step = read_policy_from_npz('gail_policy.npz', num_action=num_action)
-    print(dqn_acs_step)
-
+def visual(x, y, bins, show=True):
+    # 图表设置
     plt.rcParams['axes.unicode_minus'] = False  # 显示负号
-    plt.rcParams['figure.figsize'] = (100, 100)  # 设定图片大小
-
+    # plt.rcParams['figure.figsize'] = (50, 50)  # 设定图片大小
     f = plt.figure()  # 确定画布
-    for i in range(num_action+1):
-        # 可视化动作使用分布
-        print('action {}:'.format(i))
-        x, y = dqn_acs_step[i], gail_acs_step[i]
-        print(x)
-        print(y)
 
-        if len(x) == 0 or len(y) == 0:
-            continue
-
-        visual(i, x, y, f, bins=bins)
-
-    plt.subplots_adjust(wspace=0.2, hspace=0.5)  # 调整两幅子图的间距
-    plt.savefig('distribution_action.svg')
-    plt.show()
-
-
-def visual(i, x, y, f, bins):
     sns.set()  # 设置seaborn默认格式
     np.random.seed(0)  # 设置随机种子数
 
@@ -86,27 +48,112 @@ def visual(i, x, y, f, bins):
     dqn_kde = get_pdf_or_kde(x, bins=bins)
     gail_kde = get_pdf_or_kde(y, bins=bins)
     kl = round(kl_divergence(dqn_kde, gail_kde), 3)
-    print(kl)
 
-    f.add_subplot(3, 2, i*2+1)
+    f.add_subplot(2, 1, 1)
     plt.hist([x, y], bins=bins, range=(0, bins), density=True, align='left', label=['DQN', 'GAIL'])  # 绘制x的密度直方图
 
     plt.xlabel('Action index')
     plt.ylabel("Frequency")
     # plt.xticks(np.arange(-1, bins, 1))  # 设置x轴刻度值的字体大小
-    plt.yticks(np.arange(0.0, 1.05, 0.1))  # 设置y轴刻度值的字体大小
-    plt.title("The histogram of DQN and GAIL policy {}".format(i+1), fontsize=12)  # 设置子图标题
+    # plt.yticks(np.arange(0.0, 1.05, 0.1))  # 设置y轴刻度值的字体大小
+    plt.title("The histogram of DQN and GAIL policy", fontsize=12)  # 设置子图标题
     plt.legend()
 
-    f.add_subplot(3, 2, i*2+2)
+    f.add_subplot(2, 1, 2)
     sns.distplot(x, bins=bins, hist=False, label='DQN')  # 绘制x的密度直方图
     sns.distplot(y, bins=bins, hist=False, label='GAIL')  # 绘制y的密度直方图
     plt.xlabel('Action index')
     plt.ylabel("KDE")
     # plt.xticks(np.arange(0, bins, 10))  # 设置x轴刻度值的字体大小
     # plt.yticks(np.arange(0.0, 1.0, 0.1))  # 设置y轴刻度值的字体大小
-    plt.title("The similarity of DQN and GAIL policy {}, KL divergence={}".format(i+1, kl), fontsize=12)  # 设置子图标题
+    plt.title("The similarity of DQN and GAIL policy, KL divergence={}".format(kl), fontsize=12)  # 设置子图标题
     plt.legend()
+
+    plt.subplots_adjust(wspace=0.2, hspace=0.5)  # 调整两幅子图的间距
+    plt.savefig('distribution_action.svg')
+
+    if show:
+        plt.show()
+
+    return kl
+
+
+def parse_idx_2_action(idx):
+    time_idx, cmd_idx = idx // 9, idx % 9
+    time_cmd = 240-int(time_idx) * 15  # time cmd
+    [alt_idx, hdg_idx] = convert_with_align(cmd_idx, x=3, align=2)  # 将idx转化成三进制数
+    alt_cmd = (int(alt_idx) - 1) * 600.0  # alt cmd
+    hdg_cmd = (int(hdg_idx) - 1) * 45  # hdg cmd
+    return time_cmd, alt_cmd, hdg_cmd
+
+
+def analysis_two_actions(a1, a2):
+    cmd_list_a1 = parse_idx_2_action(a1)
+    cmd_list_a2 = parse_idx_2_action(a2)
+
+    print(a1, a2, cmd_list_a1, cmd_list_a2, a1 == a2)
+    time_diff = abs(cmd_list_a1[0] - cmd_list_a2[0]) / 240
+    alt_diff = abs(cmd_list_a1[1] - cmd_list_a2[1]) / 600
+    hdg_diff = abs(cmd_list_a1[2] - cmd_list_a2[2]) / 45
+
+    return time_diff**2+alt_diff**2+hdg_diff**2
+
+
+def visual_action_distribution():
+    bins = CmdCount
+
+    expert_path = 'dqn_policy_with_tracks.npz'
+    size = 100
+
+    # Learned Policy
+    dqn_policy = np.load(expert_path)
+    dqn_rew = dqn_policy['rews']
+    print('LP SR:', len(dqn_rew) / size * 100, np.mean(dqn_rew))
+
+    # Imitation Policy
+    gail_policy = np.load('dqn_policy_evaluate.npz')
+    gail_rew = gail_policy['rews']
+    print('IP SR:', len(gail_rew) / size * 100, np.mean(gail_rew))
+
+    dqn_acs, gail_acs = dqn_policy['acs'][:, 0], gail_policy['acs']
+    print('LP acs:', dqn_acs)
+    print('IP acs:', gail_acs)
+
+    # Action Distribution (AD)
+    kl = visual(dqn_acs, gail_acs, bins=bins, show=True)
+    print('KL divergence:', kl)
+
+    dqn_num = list(dqn_policy['num'])
+    gail_num = list(gail_policy['num'])
+    print('LP num:', dqn_num)
+    print('IP num:', gail_num)
+
+    # Matching Degree (MD)
+    num_ip_in_lp = [int(num in dqn_num) for num in gail_num]
+    num_ip_lp = sum(num_ip_in_lp)
+    num_lp = len(dqn_num)
+    print('MD: ', num_ip_lp / num_lp * 100, np.mean(num_ip_in_lp) == 1.0)
+
+    # Mean Square Error (MSE)
+    square_error_list = []
+    similarity_list = []
+    count = 0
+    for i, num in enumerate(gail_num):
+        if num not in dqn_num:
+            continue
+
+        idx = dqn_num.index(num)
+        dqn_action = int(dqn_acs[idx])
+        gail_action = int(gail_acs[i])
+        count += int(dqn_action == gail_action)
+        # Action Similarity(AS)
+        similarity = analysis_two_actions(dqn_action, gail_action)
+        similarity_list.append(similarity)
+        square_error_list.append(math.pow(dqn_action - gail_action, 2))
+
+    print('Count:', count, len(gail_acs))
+    print('MSE:', np.mean(square_error_list))
+    print('AS:', np.mean(similarity_list))
 
 
 # 随机数生成
